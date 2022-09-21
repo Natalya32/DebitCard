4760d0d6104f5438cfac08c6dd064022ec2df863
Natalya32 <tigra-pochta@mail.ru>
2022-09-21 19:56:51 +0300

    Correct test

--- src/test/java/ru/netology/rest/DebitCardTest.java
+++ src/test/java/ru/netology/rest/DebitCardTest.java
@@ -11,11 +11,11 @@ public class DebitCardTest {
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[class=\"form form_size_m form_theme_alfa-on-white\"]");
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Мамин-Сибиряк Дмитрий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
-        form.$("[class=\"button button_view_extra button_size_m button_theme_alfa-on-white\"]").click();
+        form.$("[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
